cc7b183c9865a310b229f1b8c2cabb67245f95bf
Ken Fehling <me@kenfehling.com>
2022-10-02 04:29:25 -1000

    Webpack is pain

--- webpack.config.base.babel.js
+++ webpack.config.base.babel.js
@@ -4,8 +4,9 @@ export default {
   entry: "./src/index.js",
   output: {
     library: 'react-designable-audio-player',
-    libraryTarget: 'commonjs',
+    libraryTarget: 'umd',
     globalObject: 'this',
+    umdNamedDefine: true,
     path: path.resolve('./lib'),
   },
   module: {
